0ab680769898f6f151d84c09f897754a2508b2eb
MiroshnichenkoAA <moonbangbooking@gmail.com>
2022-04-03 22:47:21 +0300

    Teleport script fixes

--- Prince of the Time/Assets/Scripts/Teleport_Enter.cs
+++ Prince of the Time/Assets/Scripts/Teleport_Enter.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Teleport_Enter : MonoBehaviour
 {
-    [SerializeField] private GameObject hero;
+    [SerializeField] private Transform hero;
     [SerializeField] private Transform exit;
+    private Vector3 offset = new Vector3(1f, 0, 0);
     public bool isTeleporting;
+    private void Start()
+    {
+        hero = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            hero.transform.position = exit.position;
+            hero.position = exit.position + offset;
             isTeleporting = true;
         }
         else isTeleporting = false;
